172990a423cb03abaf4daf43afe17e40c202e7d4
burno1 <brunofernandes100@hotmail.com.br>
2024-01-19 14:06:16 -0300

    Added simple singleton

---
+++ singleton/src/main/java/org/example/singleton/IvoryTower.java
@@ -0,0 +1,11 @@
+package org.example.singleton;
+
+public class IvoryTower {
+    private IvoryTower(){}
+
+    private static final IvoryTower INSTANCE = new IvoryTower();
+
+    public static IvoryTower getInstance(){
+        return INSTANCE;
+    }
+}
--- singleton/src/main/java/org/example/singleton/SingletonApplication.java
+++ singleton/src/main/java/org/example/singleton/SingletonApplication.java
@@ -8,6 +8,11 @@ public class SingletonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SingletonApplication.class, args);
+
+        var ivoryTower1 = IvoryTower.getInstance();
+        var ivoryTower2 = IvoryTower.getInstance();
+        System.out.println("ivoryTower1=" + ivoryTower1);
+        System.out.println("ivoryTower2=" + ivoryTower2);
     }
 
 }
