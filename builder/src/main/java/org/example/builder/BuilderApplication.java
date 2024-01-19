2697e4d2455440ea42e5dce2eb62ba1af2e8e9db
burno1 <brunofernandes100@hotmail.com.br>
2024-01-19 10:27:59 -0300

    Fixed identitation

--- builder/src/main/java/org/example/builder/BuilderApplication.java
+++ builder/src/main/java/org/example/builder/BuilderApplication.java
@@ -8,16 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BuilderApplication {
 
-
-
-
     public static void main(String[] args) {
         Castle castle = new Castle.Builder()
                 .numberOfWalls(4)
                 .build();
 
         // Displaying the details of the constructed castle
-        System.out.println("Castle with " + castle.getNumberOfWalls() + " walls");    }
+        System.out.println("Castle with " + castle.getNumberOfWalls() + " walls");
+    }
 
 
 }
